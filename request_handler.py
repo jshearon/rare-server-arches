dbea73e94e1daf05e59e6a64a809a9cd2059146b
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-11-09 22:56:31 -0600

    Create New Comment

--- request_handler.py
+++ request_handler.py
@@ -4,7 +4,7 @@ from tags import get_all_tags, get_tag_by_id, create_new_tag, update_tag, delete
 from users import get_user_by_id, get_user_by_email, create_new_user, delete_user, update_user
 from posts import get_all_posts, get_single_post, create_post, update_post, delete_post
 from categories.request import delete_category, get_all_categories, get_single_category, create_category, update_category
-from comments.request import get_all_comments
+from comments.request import create_comment, get_all_comments
 
 class HandleRequests(BaseHTTPRequestHandler):
 
@@ -106,6 +106,8 @@ class HandleRequests(BaseHTTPRequestHandler):
             new_object = create_category(post_body)
         elif resource == "tags":
             new_object = create_new_tag(post_body)
+        elif resource == "comments":
+            new_object = create_comment(post_body)
             
         self.wfile.write(f"{new_object}".encode())
 
